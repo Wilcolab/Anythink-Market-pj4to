190de5e76dbd957a69ee6dc365680fe5ab28addd
Arnon Lauden <alauden@aconex.com>
2022-04-21 00:08:15 +1000

    Seperated items creation from user creation

--- backend/seed/seed.js
+++ backend/seed/seed.js
@@ -5,24 +5,23 @@ require("../models/User");
 require("../models/Item");
 require("../models/Comment");
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URI, {poolSize: 100});
 
 let UserModel = require('mongoose').model('User');
 let ItemModel = require('mongoose').model('Item');
 const SEED_COUNT = 100;
 
+//Add users
+mongoose.connect(process.env.MONGODB_URI, {poolSize: 100});
 for (let i=0; i < SEED_COUNT; i++)
 {
   let user = new UserModel();
-  user.username = 'oldmacdonald'+i;
-  user.email = 'oldmacdonald'+i+'@gmail.com';
+  user.username = 'oldmacdonaldssss'+i;
+  user.email = 'oldmacdonaldssss'+i+'@gmail.com';
   user.setPassword('old'+i);
   user
     .save()
     .then(function (user) {
       console.log('Added user:' + user.email);
-      // return ItemModel.create(
-      //   {title: 'Cow'+i, description: 'Black cow, with '+i+' white spots', seller: user});
     }).then(function () {
       if (i==SEED_COUNT-1){
         mongoose.disconnect();
@@ -30,5 +29,21 @@ for (let i=0; i < SEED_COUNT; i++)
 
   });
 }
+
+//Add items
+mongoose.connect(process.env.MONGODB_URI, {poolSize: 100});
+for (let i=0; i < SEED_COUNT; i++) {
+  let name = 'oldmacdonaldssss' + i;
+  UserModel.findOne({username: name}).exec()
+    .then(function (user) {
+      return ItemModel.create(
+        {title: 'Cow' + i, description: 'Black cow, with ' + i + ' white spots', seller: user});
+    }).then(function () {
+    if (i == SEED_COUNT - 1) {
+      mongoose.disconnect();
+    }
+  });
+}
+
 
 console.log("done seeding");

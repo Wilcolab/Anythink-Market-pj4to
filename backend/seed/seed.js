4668c08ba188fe36efa548c84f551bd84001e238
Arnon Lauden <alauden@aconex.com>
2022-04-20 23:58:58 +1000

    removed item creation for each user

--- backend/seed/seed.js
+++ backend/seed/seed.js
@@ -5,7 +5,7 @@ require("../models/User");
 require("../models/Item");
 require("../models/Comment");
 const mongoose = require("mongoose");
-
+mongoose.connect(process.env.MONGODB_URI, {poolSize: 100});
 
 let UserModel = require('mongoose').model('User');
 let ItemModel = require('mongoose').model('Item');
@@ -13,7 +13,6 @@ const SEED_COUNT = 100;
 
 for (let i=0; i < SEED_COUNT; i++)
 {
-  mongoose.connect(process.env.MONGODB_URI, {poolSize: 10});
   let user = new UserModel();
   user.username = 'oldmacdonald'+i;
   user.email = 'oldmacdonald'+i+'@gmail.com';
@@ -22,10 +21,13 @@ for (let i=0; i < SEED_COUNT; i++)
     .save()
     .then(function (user) {
       console.log('Added user:' + user.email);
-      return ItemModel.create(
-        {title: 'Cow'+i, description: 'Black cow, with '+i+' white spots', seller: user});
+      // return ItemModel.create(
+      //   {title: 'Cow'+i, description: 'Black cow, with '+i+' white spots', seller: user});
     }).then(function () {
+      if (i==SEED_COUNT-1){
         mongoose.disconnect();
+      }
+
   });
 }
 

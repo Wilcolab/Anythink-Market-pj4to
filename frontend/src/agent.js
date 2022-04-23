0b6fa05e363fb0517ad40904339f2c6b739f97f6
Arnon Lauden <alauden@aconex.com>
2022-04-23 11:57:06 +1000

    Support optional backend-url for accessing the api server

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -6,7 +6,9 @@ const superagent = superagentPromise(_superagent, global.Promise);
 const API_ROOT =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000/api"
-    : "https://api.anythink.market/api";
+    : process.env.BACKEND_URL
+      ? process.env.BACKEND_URL
+      :"https://api.anythink.market/api";
 
 const encode = encodeURIComponent;
 const responseBody = (res) => res.body;
